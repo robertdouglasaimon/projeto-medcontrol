f14fbcae9477c05f49de0315aec66f6ee2aacce5
Robert Douglas <robertdouglasaimon@gmail.com>
2025-10-09 23:19:48 -0300

    Add cartões do painel e estilos de foco do estoque
    - Aprimoramos o painel estoque com novos cartões para “Perdas e Descarte” e “Nível de Estoque”, incluindo estilo dinâmico com base nos níveis de estoque.
    
    - Adicionamos CSS detalhado para layout de cartão e efeitos de hover.
    
    - Atualizamos o SQL para adicionar a coluna “lote_estoque” e preencher “controle_estoque” com dados em lote.
    
    - Reduzimos os dados iniciais do produto em “cadastro_produtos”.

--- medcontrol-sistema/front-end/medcontrol-sistema/telas/estoque.js
+++ medcontrol-sistema/front-end/medcontrol-sistema/telas/estoque.js
@@ -12,7 +12,43 @@ export function render () {
                 <span class="estoque-total total-estoque-valor">100</span>
             </div>
 
+            <div class="card-estoque perdas-descarte">
+                <p>
+                    <i class="fas fa-box-open"></i>
+                    Perdas e Descarte
+                </p>
+                <span class="descarte-perdas perdas-descarte-valor">300 caixas</span>
+            </div>
+
+            <div class="card-estoque nivel-estoque">
+                <p>
+                    <i class="fas fa-boxes"></i>
+                    Nivel de Estoque
+                </p>
+                <span class="estoque-nivel nivel-estoque-valor">70</span>
+            </div>
+
+        </section>
+
+        <section class="estoque-header">
+            <h2> <i class="fas fa-chart-line"></i> Gráfico de Estoque </h2>
         </section>
     `;
+
+    // Alterador de estilo do dashboard de Nivel de Estoque de acordo com a criticidade do estoque (Se for menor que o 50, fica vermelho, se for maior ou igual fica verde):
+    setTimeout(() => {
+        const card = div.querySelector('.nivel-estoque');
+        const valorSpan = div.querySelector('.nivel-estoque-valor');
+        const valorEstoque = parseInt(valorSpan.textContent);
+
+        const limiteCritico = 50;
+
+        if (valorEstoque <= limiteCritico) {
+            card.classList.add('critico');
+        } else {
+            card.classList.add('seguro');
+        }
+    }, 0);
+
     return div;
 }
