0834807db93ca588e145f8c18ccbc67576711f1f
Robert Douglas <138529257+robertdouglasaimon@users.noreply.github.com>
2025-10-23 14:38:25 -0300

    Update funcionarios.js

---
+++ medcontrol-sistema/front-end/medcontrol-sistema/telas/funcionarios.js
@@ -0,0 +1,17 @@
+export function render () {
+    const div = document.createElement("div");
+    div.classList.add("tela-funcionarios");
+    div.innerHTML = `
+    <section class="funcionarios-cards">
+        <!-- Inserir aqui os codigos dos cards de funcionarios -->
+    </section>
+
+
+
+
+
+
+    `;
+
+    return div
+}
