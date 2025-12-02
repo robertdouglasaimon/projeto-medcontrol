08140020fd5f6651aa2589d30af515968586fb5e
Robert Douglas <138529257+robertdouglasaimon@users.noreply.github.com>
2025-12-02 14:49:22 -0300

    Update login.js

--- medcontrol-sistema/front-end/medcontrol-login/src/js/login.js
+++ medcontrol-sistema/front-end/medcontrol-login/src/js/login.js
@@ -32,7 +32,7 @@ document.getElementById("form-cadastro").addEventListener("submit", async (e) =>
     console.log("🔐 Dados do funcionário:", data.funcionario);
 
     // Redireciona para o sistema
-    window.location.href = "/medcontrol-sistema/front-end/medcontrol-sistema/sistema.html#home";
+    window.location.href = "/front-end/medcontrol-sistema/sistema.html#home";
     
   } catch (error) {
     console.error("🔥 Erro no login:", error);
